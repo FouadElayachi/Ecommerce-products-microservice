55536956c4cc2c87a04cf544d817bdfc89bb2a3b
FouadElayachi <elayachifouad@gmail.com>
2020-10-13 23:48:47 +0100

    Integrate swagger for documenting our application.

--- src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -2,8 +2,11 @@ package com.ecommerce.microcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
